d9d67e0fe041793e566d74e15f2ed2fb395455dd
aomelo <andresony@gmail.com>
2017-03-06 23:55:31 +0100

    add position hierarchy handling

--- train_posrec.py
+++ train_posrec.py
@@ -1,12 +1,55 @@
 from argparse import ArgumentParser
-from create_input import load_sequences, to_no_seq
+from create_input import load_sequences, to_no_seq, pos_hier
 from lrcn import LRCN, PHImageSequenceDataGenerator, PHImageSequenceIterator
-
+from treelib import Tree, Node
+from collections import defaultdict
+import numpy as np
 
 if __name__ == '__main__':
     parser = ArgumentParser("Transforms thumbnails folder into input sequences")
     parser.add_argument("input", type=str, default=None, help="path to folder containing labels folder")
     parser.add_argument("-m", "--model", type=str, default=None, help="path to model checkpointr")
+
+    labels = ["Any","Sex","Oral","Outercourse","Noncourse"]
+    inv_labels_dict = defaultdict(lambda : set())
+    labels_dict = defaultdict(lambda : set())
+
+    labels_name_dict = {}
+    inv_labels_name_dict = {}
+
+    for node in pos_hier.all_nodes():
+        labels_name_dict[node.tag] = node.identifier
+        inv_labels_name_dict[node.identifier] = node.tag
+
+    print(labels_name_dict)
+    print(inv_labels_name_dict)
+
+    for i,label in enumerate(labels):
+        node_id = labels_name_dict[label]
+        node = pos_hier.get_node(node_id)
+        labels_dict[node.tag].add(i)
+        inv_labels_dict[i].add(node.tag)
+        print(node_id,node.tag)
+        for sub_node in pos_hier.subtree(node_id).all_nodes():
+            labels_dict[sub_node.tag].add(i)
+            inv_labels_dict[i].add(sub_node.tag)
+
+    for name, id_set in labels_dict.items():
+        print(id_set)
+        if len(id_set)>1:
+            most_specific = list(id_set)[np.argmax(np.array([pos_hier.level(id) for id in id_set]))]
+            for id in id_set:
+                if id != most_specific:
+                    inv_labels_dict[id] = inv_labels_dict[id] - inv_labels_dict[most_specific]
+        else:
+            most_specific = list(id_set)[0]
+        labels_dict[name] = most_specific
+
+
+
+    print(labels_dict)
+    print(inv_labels_dict)
+
 
     args = parser.parse_args()
 
